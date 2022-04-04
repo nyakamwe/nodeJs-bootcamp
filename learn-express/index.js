bba9247a17d24a805aac25b9c866b29bce6f409d
nyakamwe <nyakamweaimable@gmail.com>
2022-04-04 17:39:25 +0200

    setting up mongoose and doing CRUD endpoints for post

--- learn-express/index.js
+++ learn-express/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
-//creating express app
-const app =express();
+// getting routes
+const routes = require("./routes")
 
-//listening to the request on server
-app.listen(3000, ()=>{
-    console.log('Server started listening')
-});
+// Connect to MongoDB database
+
+mongoose.connect("mongodb://localhost:27017/myposts", { useNewUrlParser: true })
+	.then(() => {
+		//creating express app
+        const app =express();
+        app.use(express.json());
+        app.use("/api", routes);
 
 
+		//listening to the request on server
+        app.listen(3000, ()=>{
+            console.log('Server started listening')
+        });
+	})
